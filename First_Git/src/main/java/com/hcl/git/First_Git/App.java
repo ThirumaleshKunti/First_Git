e5376e0d4e1f01a5b31c7699a5defde3c2b28202
ThirumaleshKunti <85276172+ThirumaleshKunti@users.noreply.github.com>
2021-06-03 16:35:12 +0530

    Update App.java
    added new method for calculating product quantity.

--- First_Git/src/main/java/com/hcl/git/First_Git/App.java
+++ First_Git/src/main/java/com/hcl/git/First_Git/App.java
@@ -6,9 +6,15 @@ package com.hcl.git.First_Git;
  */
 public class App 
 {
+    public static int calculateQuantity(int UCP, int quantity){
+      return UCP * quantity;   
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World! with first eclipse git project..." );
         System.out.println( "Welcome to first git project with eclipse IDE" );
+        
+        int qty = calculateQuantity(10,30);
+        System.out.println("Calculated Quantity : "+qty);
     }
 }

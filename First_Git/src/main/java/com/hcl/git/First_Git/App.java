328d6e7dc4083d24bc4d8a2bd95dd9fc911698af
Thiru <Thiru@Java>
2021-06-03 16:26:36 +0530

    added new line for testing

--- First_Git/src/main/java/com/hcl/git/First_Git/App.java
+++ First_Git/src/main/java/com/hcl/git/First_Git/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Welcom eto first git project with eclipse IDE" );
+        System.out.println( "Hello World! with first eclipse git project..." );
+        System.out.println( "Welcome to first git project with eclipse IDE" );
     }
 }

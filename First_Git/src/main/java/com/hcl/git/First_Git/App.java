75c9517b60bec39f0b4ca2685150957a4b05b764
ThirumaleshKunti <85276172+ThirumaleshKunti@users.noreply.github.com>
2021-06-11 14:54:04 +0530

    added new method by other developer.

--- First_Git/src/main/java/com/hcl/git/First_Git/App.java
+++ First_Git/src/main/java/com/hcl/git/First_Git/App.java
@@ -1,14 +1,14 @@
 package com.hcl.git.First_Git;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static int calculateQuantity(int UCP, int quantity){
       return UCP * quantity;   
     }
+    public static void newMethod(){
+        System.out.println( "new mwthod by another developer.... " );
+        }
     public static void main( String[] args )
     {
         System.out.println( "Hello World! with first eclipse git project..." );
